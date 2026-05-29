cb777e3d36fef3ffd41cb02bba313770c87abd5e
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-05-29 23:40:49 +0700

    Initial Commit

---
+++ LongWayToGo/LongWayToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongWayToGoApp.swift
+//  LongWayToGo
+//
+//  Created by Bee Wijaya on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LongWayToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
